3ec7ce915ee50dcd6a7bccfeea71ef61a2b9b776
Anna <anna.timashova.ro@gmail.com>
2022-12-09 14:47:45 +0300

    add Homework1task4

---
+++ Homework1task4/Program.cs
@@ -0,0 +1,15 @@
+﻿int i = 1;
+int N;
+
+Console.WriteLine("Введите число:");
+    N = Convert.ToInt32(Console.ReadLine());
+
+    Console.WriteLine("Чётные числа от 1 до " + N);
+        while (i <= N)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.Write(i + ", ");
+                }
+                i++;
+            }
