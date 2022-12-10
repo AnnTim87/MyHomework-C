da030a10a23dd19312b11c7642f4e7b02a71e0b8
Anna <anna.timashova.ro@gmail.com>
2022-12-11 00:22:52 +0300

    add Homework2task13

---
+++ Homework2task13/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+int value = 39467;
+int x = value % 10;
+Console.WriteLine(value);
+if (value <=99)
+{
+    Console.WriteLine("Третьей цифры нет"); 
+}
+else
+{
+    while (value > 999)
+
+    {
+     value = value/10; 
+     x = value % 10;  
+    }
+Console.WriteLine(x);
+}
