3858c9602e8da2627786fdc16c5aa7b2901f057d
Anna <anna.timashova.ro@gmail.com>
2022-12-28 11:05:47 +0300

    add final decision

--- Finalwork/Program.cs
+++ Finalwork/Program.cs
@@ -1,54 +1,27 @@
 ﻿// Написать программу, которая из имеющегося массива строк формирует новый массив из строк, 
 // длина которых меньше, либо равна 3 символам
 
-// Метод для подсчета элементов массива определенной длины
-int CountOfElements(string[] array, int n)
+string[] array1 = new string[4] { "1234", "1567", "-2", "computer science"};
+string[] array2 = new string[array1.Length];
+void SecondArrayWithIF(string[] array1, string[] array2)
 {
     int count = 0;
-    for (int i = 0; i < array.Length; i++)
+    for (int i = 0; i < array1.Length; i++)
     {
-        if (array[i].Length <= n) count += 1;
-    }
-    return count;
-}
-
-// Метод для создания нового массива
-string[] CreateNewArray(int Length)
-{
-    return new string[Length];
-}
-
-// Метод, заполняющий массив элементами определенный длины
-string[] FillArray(string[] array, string[] newArray, int n)
-{
-    for (int i = 0; i < newArray.Length;)
-    {
-        for (int m = 0; m < array.Length; m++)
+    if(array1[i].Length <= 3)
         {
-            if (array[m].Length <= n) 
-            {
-                newArray[i] = array[m];
-                i++;
-            }
+        array2[count] = array1[i];
+        count++;
         }
     }
-    return newArray;
 }
-
-// Метод, печатающий новый массив
-string PrintArray(string[] array)
+void PrintArray(string[] array)
 {
-    string result = "[ ";
     for (int i = 0; i < array.Length; i++)
     {
-        result += $"{array[i]}";
-        if (i < array.Length - 1) result += ", ";
+        Console.Write($"{array[i]} ");
     }
-    return result + " ]";
+    Console.WriteLine();
 }
-
-// Клиентский код
-string[] lastArray = {"1234", "1567", "-2", "computer science"};
-int n = 3;
-string[] newArray = CreateNewArray(CountOfElements(lastArray, n));
-Console.WriteLine(PrintArray(FillArray(lastArray, newArray, n)));
+SecondArrayWithIF(array1, array2);
+PrintArray(array2);
